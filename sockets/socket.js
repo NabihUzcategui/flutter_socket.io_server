01bf0618a5a6aa6d3839c1ac688e5046dcd5abde
nabih uzcategui <nabihuzcateguip@gmail.com>
2023-05-10 16:33:26 -0300

    Se mejora el formato

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,20 +1,20 @@
-const {io} = require('../index');
-const Band = require('../models/band');
-const Bands = require('../models/bands');
+const { io } = require("../index");
 
-const bands = new Bands;
+const Bands = require("../models/bands");
+const Band = require("../models/band");
 
-bands.addBand( new Band('Queen'));
-bands.addBand( new Band('Pink Floyd'));
-bands.addBand( new Band('Rush'));
-bands.addBand( new Band('Silvio Rodriguez'));
+const bands = new Bands();
 
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Pink Floyd"));
+bands.addBand(new Band("Rush"));
+bands.addBand(new Band("Silvio Rodriguez"));
 
 //Mensajes de Sockets
-io.on("connection", client => {
+io.on("connection", (client) => {
   console.log("cliente conectado");
 
-  client.emit('active-bands', bands.getBands());
+  client.emit("active-bands", bands.getBands());
 
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
@@ -22,28 +22,23 @@ io.on("connection", client => {
 
   client.on("mensaje", (payload) => {
     console.log("mensaje", payload);
-
-    io.emit("mensaje", { admin: "Neuvo mensaje" });
+    io.emit("mensaje", { admin: "Nuevo mensaje" });
   });
 
-  client.on('vote-band', (payload) => {
-
+  client.on("vote-band", (payload) => {
     bands.voteBand(payload.id);
-    io.emit('active-bands', bands.getBands());
-  }); 
+    io.emit("active-bands", bands.getBands());
+  });
 
-  client.on('add-band', (payload) => {
+  client.on("add-band", (payload) => {
     const newBand = new Band(payload.name);
     bands.addBand(newBand);
-    io.emit('active-bands', bands.getBands());
-  }); 
+    io.emit("active-bands", bands.getBands());
+  });
 
-  client.on('delete-band', (payload) => {
-
+  client.on("delete-band", (payload) => {
     bands.deleteBand(payload.id);
-    io.emit('active-bands', bands.getBands());
-  }); 
-
-
-
+    io.emit("active-bands", bands.getBands());
+  });
+  
 });
